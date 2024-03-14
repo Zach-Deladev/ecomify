f8dbd5398076beb907e6becbdd739c5cc9c8fb46
Zach Del <zach.delapenha.developer@gmail.com>
2024-03-14 12:36:09 +0000

    added broswer router to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 import Home from "./pages/Home";
+import ProductPage from "./pages/ProductPage";
+import Shop from "./pages/Shop";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 const App = () => {
-  return <Home />;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+        <Routes>
+          <Route path="/productPage" element={<ProductPage />}></Route>
+        </Routes>
+        <Routes>
+          <Route path="/shop" element={<Shop />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
