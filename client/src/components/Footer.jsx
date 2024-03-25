7ecd4bcd3ace89cbda63edfbae3d20d499625768
Oliver Ball <113358642+OliverBall@users.noreply.github.com>
2024-03-25 13:01:42 +0000

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col, Button, Dropdown } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "footer.css";
+import "./footer.css";
 
 function Footer() {
   return (
