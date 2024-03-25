91c0c8dcac7566de50fd7774e3d77df7c7d7d304
Zach Del <zach.delapenha.developer@gmail.com>
2024-03-25 20:19:12 +0000

    Added product model, controller, and model to server.

---
+++ server/src/controllers/productController.js
@@ -0,0 +1,61 @@
+import Product from "../models/productModel.js";
+
+// Create a new product
+export const createProduct = async (req, res) => {
+  try {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Get a list of all products
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+// Get a single product by id
+export const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+// Update a product by id
+export const updateProduct = async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Delete a product by id
+export const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
--- server/src/server.js
+++ server/src/server.js
@@ -6,8 +6,7 @@ import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
-
-
+import productRoutes from "./routes/productRoutes.js";
 dotenv.config();
 const port = process.env.PORT || 5000;
 
@@ -31,7 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 app.options("*", cors());
 
 app.use("/api/users", userRoutes);
-
+app.use("/api/products", productRoutes);
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
